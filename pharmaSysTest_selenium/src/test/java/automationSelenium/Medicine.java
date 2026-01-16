dc936af16884e79090ff035278c0109353bf2293
Samitha Athurupana <athurupana1998@gmail.com>
2026-01-17 03:55:43 +0530

    Medicine page selenium

--- pharmaSysTest_selenium/src/test/java/automationSelenium/Medicine.java
+++ pharmaSysTest_selenium/src/test/java/automationSelenium/Medicine.java
@@ -35,10 +35,34 @@ public class Medicine {
         editMedicine.click();
         WebElement addMedicine = driver.findElement(By.id("medName"));
         addMedicine.sendKeys("Medicine Name");
+        WebElement addCategory = driver.findElement(By.id("category"));
+        addCategory.sendKeys("Category");
+        WebElement batchNumber = driver.findElement(By.id("batchId"));
+        batchNumber.sendKeys("123");
+        WebElement retailPrice = driver.findElement(By.id("price"));
+        retailPrice.sendKeys("99999");
+        WebElement expiryDate = driver.findElement(By.id("expiryDate"));
+        expiryDate.clear();
+        expiryDate.sendKeys("2026-12-31");
+        WebElement submitMedicine = driver.findElement(By.className("px-4"));
+        submitMedicine.click();
 
         // Edit Medicine
-//        WebElement editButton = driver.findElement(By.className("btn-action"));
-//        editButton.click();
+        WebElement editButton = driver.findElement(By.className("btn-action"));
+        editButton.click();
+        WebElement editMedicine1 = driver.findElement(By.id("medName"));
+        editMedicine1.sendKeys("Medicine Name");
+        WebElement editCategory = driver.findElement(By.id("category"));
+        editCategory.sendKeys("Category");
+        WebElement editbatchNumber = driver.findElement(By.id("batchId"));
+        editbatchNumber.sendKeys("123");
+        WebElement editretailPrice = driver.findElement(By.id("price"));
+        editretailPrice.sendKeys("99999");
+        WebElement editexpiryDate = driver.findElement(By.id("expiryDate"));
+        editexpiryDate.clear();
+        editexpiryDate.sendKeys("2026-12-31");
+        WebElement editSubmitMedicine = driver.findElement(By.className("px-4"));
+        editSubmitMedicine.click();
 
 
     }
