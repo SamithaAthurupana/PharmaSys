c2e56368d3267d641f515a9729162f4ad33d9dca
Samitha Athurupana <athurupana1998@gmail.com>
2026-01-17 03:15:02 +0530

    Selenium check Login

---
+++ pharmaSysTest_selenium/src/test/java/automationSelenium/login.java
@@ -0,0 +1,26 @@
+package automationSelenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class login {
+    public static void main(String[] args) {
+        WebDriverManager.edgedriver().setup();
+        WebDriver driver = new EdgeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("http://127.0.0.1:5500/frontend/login.html");
+
+        WebElement usernameTextBox = driver.findElement(By.id("username"));
+        usernameTextBox.sendKeys("admin");
+
+        WebElement passwordTextBox = driver.findElement(By.id("password"));
+        passwordTextBox.sendKeys("admin123");
+
+        WebElement loginButton = driver.findElement(By.id("loginBtn"));
+        loginButton.click();
+}
+}
