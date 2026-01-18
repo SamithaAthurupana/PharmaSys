dc50d58f6e08c2f045b7c429752b1ec2b03178f7
Samitha Athurupana <athurupana1998@gmail.com>
2026-01-18 23:51:48 +0530

    check employee page redirect and buttons using selenium

---
+++ pharmaSysTest_selenium/src/test/java/automationSelenium/Sales.java
@@ -0,0 +1,44 @@
+package automationSelenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+
+public class Sales {
+    public static void main(String[] args) {
+        WebDriverManager.edgedriver().setup();
+        WebDriver driver = new EdgeDriver();
+        // open the login page
+        driver.manage().window().maximize();
+        driver.get("http://127.0.0.1:5500/frontend/login.html");
+        // fill the login user form
+        WebElement usernameTextBox = driver.findElement(By.id("username"));
+        usernameTextBox.sendKeys("admin");
+        WebElement passwordTextBox = driver.findElement(By.id("password"));
+        passwordTextBox.sendKeys("admin123");
+        WebElement loginButton = driver.findElement(By.id("loginBtn"));
+        loginButton.click();
+
+        // Wait until dashboard loads
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlContains("dashboard"));
+        // Open employee page
+        driver.manage().window().maximize();
+        driver.get("http://127.0.0.1:5500/frontend/employee.html");
+
+        // click Add employee and close it
+        WebElement updateInventoryBTN =
+                wait.until(ExpectedConditions.elementToBeClickable(By.className("px-4")));
+        updateInventoryBTN.click();
+        WebElement closeInventoryBTN =
+                wait.until(ExpectedConditions.elementToBeClickable(By.className("btn-close")));
+        closeInventoryBTN.click();
+    }
+}
