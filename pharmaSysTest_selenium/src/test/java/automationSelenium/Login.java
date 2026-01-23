9296cc0a423acfa47ed82cbda5ea6f9183407e47
Samitha Athurupana <athurupana1998@gmail.com>
2026-01-23 18:27:25 +0530

    commit change comments

--- pharmaSysTest_selenium/src/test/java/automationSelenium/Login.java
+++ pharmaSysTest_selenium/src/test/java/automationSelenium/Login.java
@@ -10,16 +10,16 @@ public class Login {
     public static void main(String[] args) {
         WebDriverManager.edgedriver().setup();
         WebDriver driver = new EdgeDriver();
-
+        // login in to login page
         driver.manage().window().maximize();
         driver.get("http://127.0.0.1:5500/frontend/login.html");
-
+        // add credentials
         WebElement usernameTextBox = driver.findElement(By.id("username"));
         usernameTextBox.sendKeys("admin");
 
         WebElement passwordTextBox = driver.findElement(By.id("password"));
         passwordTextBox.sendKeys("admin123");
-
+        // click login
         WebElement loginButton = driver.findElement(By.id("loginBtn"));
         loginButton.click();
 }
